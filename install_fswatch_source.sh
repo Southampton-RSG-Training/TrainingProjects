aa053dd9907838a43a2218f4dbe8f4bda046852a
PipGrylls <pipgryllsastro@gmail.com>
2022-12-06 10:13:56 +0000

    windows fswatch install

---
+++ install_fswatch_source.sh
@@ -0,0 +1,9 @@
+# Installing this will require GCC if it is installed then the reload via Rsync can be harmonised.
+
+mkdir -p install_fswatch
+cd install_fswatch
+
+curl -L -O https://github.com/emcrisostomo/fswatch/releases/download/1.17.1/fswatch-1.17.1.tar.gz --output fswatch-1.17.1.tar.gz
+
+tar -xvzf fswatch-1.17.1.tar.gz
+
