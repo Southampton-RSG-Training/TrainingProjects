15cb724628dd4c2fedbeb6f3c7fc0c71ee72b87f
PipGrylls <pipgryllsastro@gmail.com>
2022-12-08 12:58:02 +0000

    adding dry run and correcting rsync flag

--- dry-run-build-local.sh
+++ dry-run-build-local.sh
@@ -21,12 +21,14 @@ else
       echo "Unknown OS, $OSTYPE, please review lines 1 to 14"
 fi
 
+echo "building on $OSTYPE, using simple type $simple_os_type"
+
 if [ "$1" = "" ]; then
     echo "First argument must be path to lesson to build"
     exit
 fi
 
-echo "Building and deploying $1 to Vagrant"
+echo "Dry run of $1 to Vagrant"
 
 cd VagrantBuild
 
@@ -36,21 +38,21 @@ IFS=',' read -ra state_array <<< $vagrant_state
 if [ "${state_array[3]}" != "running" ];
 then
   echo "Vagrant not running, bringing up, if unprovisioned this may take a few minutes"
-  vagrant up
+  echo vagrant up
 fi
 
 # Gather information about the vagrant instance
-vagrant ssh-config > .ssh_info.tmp
+echo vagrant ssh-config > .ssh_info.tmp
 
-lessonbox_ip=$(grep 'HostName' .ssh_info.tmp | cut -d " " -f 4)
-lessonbox_port=$(grep 'Port' .ssh_info.tmp | cut -d " " -f 4)
-lessonbox_user=$(grep -w 'User' .ssh_info.tmp | cut -d " " -f 4)
+echo lessonbox_ip=$(grep 'HostName' .ssh_info.tmp | cut -d " " -f 4)
+echo lessonbox_port=$(grep 'Port' .ssh_info.tmp | cut -d " " -f 4)
+echo lessonbox_user=$(grep -w 'User' .ssh_info.tmp | cut -d " " -f 4)
 
 
-vagrant plugin install vagrant-scp
+echo vagrant plugin install vagrant-scp
 
 # Snapshot the VM blank state to restore later
-vagrant snapshot push
+echo vagrant snapshot push
 
 echo "matching path: $1"
 # match upto the first fwd slash
@@ -61,10 +63,8 @@ if [[ $1 =~ $re ]]; then
 fi
 
 echo ssh -c "mkdir -p ~/Materials/$containing_dir"
-vagrant ssh -c "mkdir -p ~/Materials/$containing_dir"
 # Copy the files required to build the lesson to the vm.
 echo scp ../$1 LessonBox:~/Materials/$1
-vagrant scp ../$1 LessonBox:~/Materials/$1
 
 # get absolute path to the input episodes
 get_abs_filename() {
@@ -80,61 +80,61 @@ else
   rsync_on=0
 fi
 
+echo "rsync status $rsync_on"
+
 if [[ $rsync_on == 1 ]]; then
   abs_path=$(get_abs_filename ../$ep_path)
 
   # remove files from intended mount point
-  vagrant ssh -c "rm ~/Materials/$ep_path*; rm ~/Materials/$ep_path.gitkeep"
+  echo vagrant ssh -c "rm ~/Materials/$ep_path*; rm ~/Materials/$ep_path.gitkeep"
 
-  mkdir -p tmp
+  echo mkdir -p tmp
 
   if [[ $simple_os_type == "WIN" ]]; then
       # Windows
       # use a template to create a task to pass into the scheduler
       # Windows run this hack
-      echo "rsync -r $abs_path/ $lessonbox_user@$lessonbox_ip:~/Materials/$ep_path -e 'ssh -p $lessonbox_port -i .vagrant/machines/LessonBox/virtualbox/private_key'" > tmp/rsync_task.sh
-      bash rsync_while_loop.sh & >> tmp/rsync_log.txt
-      pid_for_hack=$!
-      echo we are going to kill $pid_for_hack later
+      echo echo "rsync -r $abs_path/ $lessonbox_user@$lessonbox_ip:~/Materials/$ep_path -e 'ssh -p $lessonbox_port -i .vagrant/machines/LessonBox/virtualbox/private_key'" > tmp/rsync_task.sh
+      echo bash rsync_while_loop.sh & >> tmp/rsync_log.txt
+      echo pid_for_hack=$!
+      echo echo we are going to kill $pid_for_hack later
   else
     # cron can't run every 5 seconds so we will switch to inotify
     # Sane OSes, start an file watch using inotify and on change run:
-    rsync_command="rsync -r $abs_path/ $lessonbox_user@$lessonbox_ip:~/Materials/$ep_path -e 'ssh -p $lessonbox_port -i .vagrant/machines/LessonBox/virtualbox/private_key'"
-    rsync -r $abs_path/ $lessonbox_user@$lessonbox_ip:~/Materials/$ep_path -e 'ssh -p $lessonbox_port -i .vagrant/machines/LessonBox/virtualbox/private_key'
-    echo "fswatch -0 -r -i -o '.*\.md\$' --event 14 $abs_path/ | xargs -0 -n 1 -I {} $rsync_command" > tmp/fswatch_task.sh
-    bash tmp/fswatch_task.sh &
-    pid_for_fswatch=$!
-    echo we will kill $pid_for_fswatch later
+    echo rsync_command="rsync -r $abs_path/ $lessonbox_user@$lessonbox_ip:~/Materials/$ep_path -e 'ssh -p $lessonbox_port -i .vagrant/machines/LessonBox/virtualbox/private_key'"
+    echo rsync -r $abs_path/ $lessonbox_user@$lessonbox_ip:~/Materials/$ep_path -e 'ssh -p $lessonbox_port -i .vagrant/machines/LessonBox/virtualbox/private_key'
+    echo echo "fswatch -0 -r -i -o '.*\.md\$' --event 14 $abs_path/ | xargs -0 -n 1 -I {} $rsync_command" > tmp/fswatch_task.sh
+    echo bash tmp/fswatch_task.sh &
+    echo pid_for_fswatch=$!
+    echo echo we will kill $pid_for_fswatch later
   fi
 fi
 
-sleep 10
+echo sleep 10
 
 # Do the RSG python/R pre build, the sed command is required to make deployment from windows work.
-echo ssh -c "cd ~/Materials/$1; pwd; sed -i 's/\r$//' bin/build_me.sh; bash bin/build_me.sh"
-vagrant ssh -c "cd ~/Materials/$1; pwd; sed -i 's/\r$//' bin/build_me.sh; bash bin/build_me.sh"
+echo vagrant ssh -c "cd ~/Materials/$1; pwd; sed -i 's/\r$//' bin/build_me.sh; bash bin/build_me.sh"
 
 ## Build the site.
-echo ssh -c "cd ~/Materials/$1; bundler install; bundler exec jekyll serve --baseurl='' --host=0.0.0.0 --port=8124 --livereload --livereload-port=8125"
-vagrant ssh -c "cd ~/Materials/$1; bundler install; bundler exec jekyll serve --baseurl='' --host=0.0.0.0 --port=8124 --livereload --livereload-port=8125"
+echo vagrant ssh -c "cd ~/Materials/$1; bundler install; bundler exec jekyll serve --baseurl='' --host=0.0.0.0 --port=8124 --livereload --livereload-port=8125"
 
 
-read -p 'Press any key to bring the test server down restore the snapshot'
+echo read -p 'Press any key to bring the test server down restore the snapshot'
 
 if [[ $rsync_on == 1 ]]; then
   # Kill the reload
   if [[ $simple_os_type == "WIN" ]]; then
       # In windows we need to kill the hack
-      kill $pid_for_hack
+      echo kill $pid_for_hack
   else
       # In sane OS we need to kill the watch
-      kill $pid_for_fswatch
+      echo kill $pid_for_fswatch
   fi
-  rm tmp/rsync_task.sh tmp/rsync_log.txt tmp/fswatch_task.sh
+  echo rm tmp/rsync_task.sh tmp/rsync_log.txt tmp/fswatch_task.sh
 fi
 
-rm .ssh_info.tmp
-rmdir tmp
+echo rm .ssh_info.tmp
+echo rmdir tmp
 
 # Restore the vm to the snapshot
-vagrant snapshot pop
+echo vagrant snapshot pop
